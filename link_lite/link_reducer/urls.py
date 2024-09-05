8b192b6dd8c47ff47bea8e281f8dd5ff9097d487
Antoine Stouff <antoine-78280@hotmail.fr>
2024-09-05 11:05:39 +0200

    feat/ redirection generic route and logic

--- link_lite/link_reducer/urls.py
+++ link_lite/link_reducer/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('shorten-url', views.shorten_url, name='shorten_url'),
     path('list-user-urls', views.get_all_urls, name='list_urls'),
+    path('<str:short_url>', views.redirect_url, name='redirect_url'),
 ]
