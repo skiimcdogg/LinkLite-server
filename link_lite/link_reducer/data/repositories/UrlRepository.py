f107913820ff05a30c8a263788c3dcb499a9e524
Antoine Stouff <antoine-78280@hotmail.fr>
2024-09-06 14:34:49 +0200

    feat/Add the logic for reduce urls

--- link_lite/link_reducer/data/repositories/UrlRepository.py
+++ link_lite/link_reducer/data/repositories/UrlRepository.py
@@ -1,3 +1,15 @@
+from ...models import URL
+
+
 class UrlRepository:
     def get_all():
         pass
+
+    @staticmethod
+    def save(original_url, short_code, user=None):
+        return URL.objects.create(original_url=original_url, short_url=short_code, user=user)
+    
+    @staticmethod
+    def exists_by_short_code(short_code):
+        return URL.objects.filter(short_url=short_code).exists()
+    
--- link_lite/link_reducer/views/views.py
+++ link_lite/link_reducer/views/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from ..models import URL
+from ..services.UrlShortenService import UrlShortenService
 
 def shorten_url(request):
-    return HttpResponse("Hello")
+    if request.method == 'POST':
+        original_url = request.POST.get('original_url')
+        user = request.user if request.user.is_authenticated() else None
+
+        short_url = UrlShortenService.create_short_url(original_url, user)
+
+    return HttpResponse(f"Short URL created -> {short_url}")
 
 def get_all_urls(request):
     return HttpResponse("Hello")
